1b3c0c82815b55dc68a0af57a623e405294bc66f
PappP <PappP@202-11>
2025-03-06 18:35:38 +0100

    Add project files.

---
+++ mrq/Controllers/ProductsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using mrq.Models;
+
+namespace mrq.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult> GetAll()
+        {
+            using (var context = new WebstoreContext())
+            {
+                var product = await context.Products.Include(x=> x.Category).ToListAsync();
+
+                return Ok(new { result = product, message = "Sikeres lekérdezés." });
+            }
+        }
+    }
+}
